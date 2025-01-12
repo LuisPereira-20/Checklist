244db8c7f76405a751b4df037722beae659859a6
LuisPereira-20 <151740701+LuisPereira-20@users.noreply.github.com>
2025-01-12 09:42:35 -0400

    check

--- checklist.js
+++ checklist.js
@@ -5,14 +5,19 @@ const lista_tarea = document.getElementById("lista");
 let tareas = [];
 
 agregar.addEventListener("click", () => {
-    const tarea = nueva_tarea.value;
-    if (tarea){
-        tareas.push(tarea);
+    const new_tarea = nueva_tarea.value;
+    if (new_tarea){
+        tareas.push(new_tarea);
         reinicio();
         nueva_tarea.value = "";
     }
 });
 
 function reinicio () {
-    
+    lista_tarea.innerHTML = "";
+    tareas.forEach((tarea) => {
+        const list = document.createElement ("li");
+        list.textContext = tarea;
+        lista_tarea.appendChild(list)
+    });
 }
